6a3d1719f264bd504d05fa4a5be1bea18727158e
James Cole <james@assimityai.com>
2022-07-20 20:33:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# pastel_colours
+
+A selection of pastel colours for [termion](https://github.com/redox-os/termion).
+
+They look like this:
+![All pastel colours](examples/all.png)
+
+You can see these in your own terminal by running the example:
+```shell
+cargo run --example all
+```
+
+## History
+
+Developed for us in [lk](https://github.com/jamescoleuk/lk) and [fuzzy_finder](https://github.com/jamescoleuk/fuzzy_finder).
---
+++ examples/all.rs
@@ -0,0 +1,24 @@
+use pastel_colours::*;
+
+fn main() {
+    println!("BLUE_BG:{BLUE_BG}Blue background{RESET_FG}{RESET_BG}");
+    println!("BLUE_FG:{BLUE_FG}Blue foreground{RESET_FG}{RESET_BG}");
+    println!("DARK_BLUE_BG:{DARK_BLUE_BG}Dark blue background{RESET_FG}{RESET_BG}");
+    println!("DARK_BLUE_FG:{DARK_BLUE_FG}Dark blue foreground{RESET_FG}{RESET_BG}");
+    println!("DARK_GREEN_BG:{DARK_GREEN_BG}Dark green background{RESET_FG}{RESET_BG}");
+    println!("DARK_GREEN_FG:{DARK_GREEN_FG}Dark green foreground{RESET_FG}{RESET_BG}");
+    println!("DARK_GREY_BG:{DARK_GREY_BG}Dark grey background{RESET_FG}{RESET_BG}");
+    println!("DARK_GREY_FG:{DARK_GREY_FG}Dark grey foreground{RESET_FG}{RESET_BG}");
+    println!("GREEN_BG:{GREEN_BG}Green background{RESET_FG}{RESET_BG}");
+    println!("GREEN_FG:{GREEN_FG}Green foreground{RESET_FG}{RESET_BG}");
+    println!("GREY_BG:{GREY_BG}Grey background{RESET_FG}{RESET_BG}");
+    println!("GREY_FG:{GREY_FG}Grey foreground{RESET_FG}{RESET_BG}");
+    println!("ORANGE_BG:{ORANGE_BG}Orange background{RESET_FG}{RESET_BG}");
+    println!("ORANGE_FG:{ORANGE_FG}Orange foreground{RESET_FG}{RESET_BG}");
+    println!("PINK_BG:{PINK_BG}Pink background{RESET_FG}{RESET_BG}");
+    println!("PINK_FG:{PINK_FG}Pink foreground{RESET_FG}{RESET_BG}");
+    println!("RED_BG:{RED_BG}Red backround{RESET_FG}{RESET_BG}");
+    println!("RED_FG:{RED_FG}Red foreground{RESET_FG}{RESET_BG}");
+    println!("TEAL_BG:{TEAL_BG}Teal background{RESET_FG}{RESET_BG}");
+    println!("TEAL_FG:{TEAL_FG}Teal foreground{RESET_FG}{RESET_BG}");
+}
